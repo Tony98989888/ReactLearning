ab825670d38bdb521c509365210661fdf845a283
Tony <649072815@qq.com>
2023-07-18 10:40:45 +0800

    First commit

---
+++ src/Components/ClassComponent.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+class ClassComponent extends React.Component
+{
+    render()
+    {
+        return <p>This is a class component</p>
+    }
+}
+
+export default ClassComponent
